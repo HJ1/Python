f35bb51c968e5fceda9c69a12bfb2817f9914881
HJ1 <Hj862@hotmail.com>
2016-01-20 04:55:11 +0100

    Platformer Project 20/01/16

--- Pygame/Platformer Project/Platformer Project.py
+++ Pygame/Platformer Project/Platformer Project.py
@@ -2,3 +2,21 @@
 import pygame, pygame._view, sys, os
 from pygame.locals import *
 pygame.init()
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.display.set_caption("Platformer Project")
+pygame.mouse.set_visible(0)
+window_height = 800
+window_length = 800
+pygame.display.set_mode((window_height, window_length))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+    pygame.display.update()
+
+pygame.quit()
